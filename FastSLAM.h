d17957cbe381f0092069e8550dce390b41c5f9a4
LiliMeng <lilimeng1103@gmail.com>
2015-10-05 18:51:12 -0700

    Update FastSLAM.h

--- FastSLAM.h
+++ FastSLAM.h
@@ -1,1 +1,64 @@
+#ifndef FASTSLAM_H
+#define FASTSLAM_H
 
+#include <vector>
+#include <string>  
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <cmath> 
+#include <Eigen/Core>
+#include <Eigen/Dense>
+
+using namespace std;
+using namespace Eigen;
+
+
+class FastSLAM{
+	public:
+	
+	struct Sensor
+	{
+		int id;
+		double r;
+		double theta;
+	};
+	
+	struct landmark
+	{
+		bool observed;
+		MatrixXd mu;
+		MatrixXd sigma;
+		
+     };
+     
+	struct Odometry
+	{
+		double r1;
+		double t;
+		double r2;
+		vector<Sensor> sensorVec;
+
+	};
+	
+	struct Particle
+	{
+		double weight;
+		double poseX;
+		double poseY;
+		double poseTheta;
+		landmark landmarks[9];
+      };
+	
+     vector<Particle> particles;
+     vector<Particle> newParticles;
+     
+     double NormalAngle(double angle);
+     
+     void fastSLAM();
+     
+	
+};
+
+
+#endif // FASTSLAM_H
